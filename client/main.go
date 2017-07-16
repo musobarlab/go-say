4dc9fd359c243294a5272ca84347a09ccd098c35
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-07-16 13:58:11 +0700

    fixing error args

--- client/main.go
+++ client/main.go
@@ -17,7 +17,7 @@ func main() {
 	output := flag.String("o", "output.wav", "wav output")
 	flag.Parse()
 
-	if len(os.Args) < 2 {
+	if flag.NArg() < 1 {
 		fmt.Printf("usage:\n\t%s \"text to Speech\"", os.Args[0])
 		os.Exit(1)
 	}
@@ -30,7 +30,7 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewTextToSpeechClient(conn)
-	text := &pb.Text{Text: os.Args[1]}
+	text := &pb.Text{Text: flag.Arg(0)}
 	res, err := client.Say(context.Background(), text)
 	if err != nil {
 		log.Fatal(err)
